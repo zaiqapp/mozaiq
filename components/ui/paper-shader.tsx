01e0958d975c5a62f1aa44ba2399ba641d38249f
cadesarkin <sarkincade@gmail.com>
2026-03-25 23:07:49 -0500

    feat: add PaperShader fixed background component

---
+++ components/ui/paper-shader.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { GrainGradient } from '@paper-design/shaders-react'
+
+export function PaperShader() {
+  return (
+    <GrainGradient
+      style={{
+        position: 'fixed',
+        inset: 0,
+        zIndex: -1,
+        pointerEvents: 'none',
+        width: '100%',
+        height: '100%',
+      }}
+      colorBack="#080810"
+      colors={['hsl(190,100%,45%)', 'hsl(245,80%,60%)', 'hsl(220,30%,15%)']}
+      noise={0.2}
+      softness={0.7}
+      shape="blob"
+    />
+  )
+}
